5306cb631539d4a0b432eadf99bb91fa8acf1d98
Junjie Liang <junjieliang@gmail.com>
2011-07-30 03:00:37 -0700

    Incomplete checkout, not tested

--- dispatch.py
+++ dispatch.py
@@ -3,20 +3,23 @@ import yaml
 import pprint as pp
 import lockfile
 import urllib2
+import tempfile
 
 FILENAME = ".dotmagic.yaml"
-LOCKFILE = ".dotmagic/GLOBAL.LOCK"
+LOCKFILE = "GLOBAL.LOCK"
 
 CONFIG = {}
+homepath = os.getenv('HOME')
+magicpath = os.path.join(homepath, ".dotmagic")
 
 def run():
+    global CONFIG
 
     # read .dotmagic.yaml
     # load the yaml file
 
     env_map = {}
 
-    homepath = os.getenv('HOME')
     path = os.path.join(homepath, FILENAME) 
 
     f = open(path)
@@ -25,7 +28,7 @@ def run():
     CONFIG = doc
     f.close()
 
-    pp.pprint(doc)
+    pp.pprint(CONFIG)
 
 
     # parse the cmd line args
@@ -43,34 +46,107 @@ def run():
     elif cmd == 'fetch':
         fetch(param)
     elif cmd == 'checkout':
-        pass
+        checkout(param)
     elif cmd == 'try':
         pass
 
     
 
 def fetch(user):
-    homepath = os.getenv('HOME')
-    lockpath = os.path.join(homepath, ".dotmagic", "GLOBAL.LOCK")
-    lock = FileLock(lockpath)
+    global CONFIG
+
+    lockpath = os.path.join(magicpath, LOCKFILE)
+    lock = lockfile.FileLock(lockpath)
     
+    # acquire the global lock
     try:
         lock.acquire(timeout=5)
-    except LockTimeout:
+    except lockfile.LockTimeout:
         sys.stderr.write("dotmagic is currently running. Please try again later.\n")
         return
 
-    # lock is now acquired!
-
     # download tar file
-    infile = urllib2.urlopen("/".join([CONFIG["REPO"], "api", "fetch", user]))
+    try:
+        url = "/".join([CONFIG["REPO"], "api", "fetch", user]);
+        print("Attempting to download from %s..." % url) # TODO: do we need a progress bar?
+        infile = urllib2.urlopen(url)
+    except urllib2.URLError:
+        sys.stderr.write("Error downloading for user %s. Check your repo configuration.\n" % user)
+        return
+
     outfile = tempfile.NamedTemporaryFile()
     outfile.write(infile.read())
+    outfile.flush()
     
-    # create the output directory
-    os.system("mkdir -p ~/.dotmagic/repo/%s" % user)
-    os.system("tar -xvzf %s -C ~/.dotmagic/repo/%s" % (outfile.name, user))
+    # create the output directory, empty it, then untar into it
+    os.system("mkdir -p %s/repo/%s" % (magicpath, user))
+    os.system("rm -rf %s/repo/%s/*" % (magicpath, user))
+    os.system("tar -xvzf %s -C %s/repo/%s" % (outfile.name, magicpath, user))
     outfile.close()
 
     # now we unlock the filelock
     lock.release()
+    return
+
+def checkout(user):
+    lockpath = os.path.join(magicpath, LOCKFILE)
+    lock = lockfile.FileLock(lockpath)
+    
+    # acquire the global lock
+    try:
+        lock.acquire(timeout=5)
+    except lockfile.LockTimeout:
+        sys.stderr.write("dotmagic is currently running. Please try again later.\n")
+        return
+
+    userpath = os.path.join(magicpath, "repo", user)
+    if not os.path.exists(userpath):
+        sys.stderr.write("Could not find user %s data. Did you forget to do 'dotmagic fetch %s'?" % (user, user))
+        return
+
+    # check if we need to backup the data first
+    # if backup directory already exists, then we do not need to :)
+    backuppath = os.path.join(magicpath, "backup")
+    need_backup = not os.path.exists(backuppath)
+
+    if need_backup:
+        os.system("mkdir -p %s" % backuppath)
+
+    filelist = os.listdir(userpath)
+    print("Files in userpath: %s" % filelist)
+    unrecognized = []
+    for filename in filelist:
+        if filename == "meta.yaml":
+            continue
+
+        # assume we have the whitelist
+        #if filename in globals():
+        #    whitelist = globals()[filename].WHITELIST
+        #else:
+        #    unrecognized.append(filename)
+        #    continue
+
+        if filename != "vim":
+            continue
+
+        whitelist = [".vimrc", ".vim"]
+
+        for f in whitelist:
+            fulluserpath = os.path.join(userpath, f)
+            fullhomepath = os.path.join(homepath, f)
+            if os.path.exists(fulluserpath):
+                if os.path.isdir(fulluserpath):
+                    cmd = "cp -R %s %s"
+                else:
+                    cmd = "cp %s %s"
+
+                if need_backup:
+                    os.system(cmd % (fullhomepath, backuppath))
+
+                os.system(cmd % (fulluserpath, fullhomepath))
+
+    if unrecognized:
+        sys.stderr.write("Rcfiles for these programs could not be loaded: %s\n" % " ".join(unrecognized))
+
+    lock.release()
+    return
